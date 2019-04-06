ab9342c333bca4359a957c5cfe6c76a5ec41a9bd
abhinavds <abhinav.ds@utexas.com>
2019-04-05 21:25:18 -0500

    GCN modified for relation embeddings
    Signed-off-by: abhinavds <abhinav.ds@utexas.com>

--- src/graphQA/modules/relation_embedding.py
+++ src/graphQA/modules/relation_embedding.py
@@ -8,4 +8,13 @@ class RelationEmbedding(nn.Module):
 		super(RelationEmbedding, self).__init__()
 		self.embedding_size = embedding_size
 		self.embedding_vocabulary = embedding_vocabulary
-		# TODO: everything
+		# TODO: everything (create embedding matrix); 
+
+# TODO: create adjacency matrix; create pooling layer
+
+# Code flow
+# 1. pooling
+# 2. adjacency matrix
+# 3. relation matrix/embedding; maybe initialize with some word embedding itself?
+# 4. GCN (can use attention if needed, but maybe too much)
+# 5. Add whole image feature (very pixelated), gcn, average relation(embedding) for attention
